c5343c1b8074c8a3225b9d39bafb7d7bd9caa7d2
Eli Martin <elizabeth.k.martin-1@ou.edu>
2020-11-04 16:58:04 -0600

    created class and created an example arraylist

---
+++ src/MapExamples.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class MapExamples
+{
+	public static void main(String args[])
+	{
+		// Show how you would use Set to find the unique elements in a List.
+		
+		// creates an arraylist that stores: 1, 2, 3, 3 (repeated), 4
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1); list.add(2); list.add(3); list.add(3); list.add(4);
+		// creates a set that will store the unique elements from the list
+		
+	}
+	
+}
